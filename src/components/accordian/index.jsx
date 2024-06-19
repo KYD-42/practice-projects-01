29f4cebc35a13131bccf7dfd0666883a9defffab
KYD-42 <thekyd42@gmail.com>
2024-06-19 21:00:55 +0100

    project 1 done

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import data from "./data";
+import "./styles.css";
 // types of accordians
 // single selection
 // multiple selection
 
 export default function Accordian() {
-  // setting the state of the accordian to null by default
   const [selected, setSelected] = useState(null);
-
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
   // function to handle single selection;
   // we will pass the id of the selected item;
   // the function is updated with the id of the selected item;
   // we create an if statement to check the selected state;
   // if the item is already selected we set the selected state to null - closing the accordian;
   // if the item is not selected we set the selected state to the id of the said item - opening the accordian;
-  const handleSingleSelection = (getCurrentId) => {
+  function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
-  };
+  }
+
+   function handleMultiSelection(getCurrentId) {
+    let cpyMutiple = [...multiple];
+    const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);
+    else cpyMutiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMutiple);
+  }
 
   // logging this state of the accordian so we can see the id of the selected item in our browser's console
-  console.log(selected);
+  console.log(selected, multiple);
   // let's render the accrodian
+  // the button component is used to enable multi selection, so that we can have multiple accordians open at the same time
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
@@ -30,15 +46,23 @@ export default function Accordian() {
               <div
                 // we will pass the id of the selected item on click
                 // the function is updated with the id of the selected item
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
-                <div className="content">{dataItem.answer}</div>
-              ) : null}
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
             </div>
           ))
         ) : (
