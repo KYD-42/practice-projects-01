f2faae4b6c8614708ef63ff222f217cd99f2bc67
“Filipe.Santiago” <thekyd42@gmail.com>
2024-08-23 23:37:50 +0100

    homepage-start

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import "../pages/PagesStyles.css";
+
+export default function HomePage() {
+  return (
+    <div className="project-cards">
+      <button>accordian project</button>
+      <button>random color project</button>
+      <button>star rating project</button>
+      <button>image slider project</button>
+      <button>load more data project</button>
+    </div>
+  );
+}
